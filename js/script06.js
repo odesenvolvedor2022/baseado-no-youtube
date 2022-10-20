3b3e095fa2889df54ce8dcc543050a30209691aa
Osvaldo Bastos Teixeira Junior <115334336+odesenvolvedor2022@users.noreply.github.com>
2022-10-20 10:34:07 -0300

    fim-aula06

--- js/script06.js
+++ js/script06.js
@@ -9,30 +9,7 @@ $(document).ready(function(){
         <p>Em outras palavras é a árvore de elementos do nosso código. O jQuery pode acessar e atualizar dinamicamente o conteúdo, a estrutura e o estilo de um documento.</p>
         <p>Tudo pode ser modificado, adicionado ou removido usando jQuery.</p>
         <input id="campo_texto" type="text" placeholder="Digite algo...">
-        <a class="btn" id="btn-redirect" href="https://jquery.com">Ir para o jquery</a>
-        <hr>
-            <div id="divBotoes">
-                <button class="btn" id="trocar-img">Trocar imagem</button>
-                
-                <button class="btn" id="trocar-link">Trocar link</button>
-                
-                <button class="btn" id="atribuir-texto">Atribuir texto ao campo</button>
-
-            </div>
-
-            <hr class="separador">
-
-            <h2>Aplicação para testar o append e o prepend:</h2>
-            
-            <div id="addDivs">
-                
-                <button class="btn" id="remDiv">Prepend</button>
-                
-                <button class="btn" id="addDiv">Append</button>
-            
-            </div>
-            
-            <div id="divs"></div>`
+        <a class="btn" id="btn-redirect" href="https://jquery.com">Ir para o jquery</a>`
     );
 
     // var htmlH1 = $('h1').html();
@@ -62,31 +39,74 @@ $(document).ready(function(){
     $('#trocar-img').click(function(){
         $('img').css('transition','1s ease');
         $('img').attr('src','https://cdn.icon-icons.com/icons2/2415/PNG/512/jquery_plain_wordmark_logo_icon_146445.png').css('transform','scale(1.1)');
+        $(this).hide('fast');
     });
 
     // TROCAR O LINK E O TEXTO DO BOTÃO DE REDIRECIONAMENTO
     $('#trocar-link').click(function(){
         $('#btn-redirect').text('Ir para o google');
         $('#btn-redirect').attr('href','https://google.com');
+        $(this).hide('fast');
     });
 
     // ATRIBUIR TEXTO AO CAMPO
     $('#atribuir-texto').click(function(){
-        $('#campo_texto').val('Texto atribuido com um click!');
+        $('#campo_texto').val('Texto atribuído com um clique!');
+        $(this).hide('fast');
         var campoTexto = $('#campo_texto').val();
-        alert(campoTexto);
+        $('#campo_texto').focus();
     });
 
     // append (adicionar após) 20:30 min
-    $('#addDiv').click(function(){
+    $('#append').click(function(){
         $('#divs').append('<div class="append new"></div>');
     });
 
     // prepend (adiocionar antes)
-    $('#remDiv').click(function(){
+    $('#prepend').click(function(){
         $('#divs').prepend('<div class="prepend new"></div>')
     });
 
-    // REMOVER ELEMENTOS 24:30 min
+    // ZERAR
+    // Apagar todas as divs adicionadas com o metodo empty()
+    $('#zerarDivs').click(function(){
+        $('#divs').empty('slow');
+    });
+
+    // REMOVER ELEMENTOS
+    // remove() 24:30 min
+    $('#removerDivs').click(function(){
+        $('.new:last-child').remove();
+    });
+
+    // MANIPULANDO O CSS
+    // addClass(), removeClass() e toggleClass()
+    $('#toggleTema').click(function(){
+        $('body').toggleClass('light');
+        $('body').css('transition','.3s ease');
+        var classBody = $('body').attr('class');
+
+        if(classBody == 'light'){
+            $(this).text('Dark');
+        }else{
+            $(this).text('Light');
+        };
+        
+        var caminhoImg = $('img').attr('src');
+        
+        if(caminhoImg == 'https://cdn.icon-icons.com/icons2/2415/PNG/512/jquery_plain_wordmark_logo_icon_146445.png'){
+            $('img').attr('src','https://icon-library.com/images/jquery-icon-png/jquery-icon-png-21.jpg');
+        }else{
+            $('img').attr('src','https://cdn.icon-icons.com/icons2/2415/PNG/512/jquery_plain_wordmark_logo_icon_146445.png');
+        };
+    });
+
+    // PEGANDO O TAMANHO DE UM ELEMENTO
+    // .width() e .height()
+    $('img').mouseleave(function(){
+        var larguraImg = $(this).width();
+        var alturaImg = $(this).height();
+        alert('A largura desta imagem é de ' + larguraImg + 'px e a altura é de ' + alturaImg + 'px.');
+    });
 
 });
